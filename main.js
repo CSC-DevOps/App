368f7a53c5287e29e7ea6cc37eb16e0d53bcb731
Chris Parnin <chris.parnin@gmail.com>
2015-03-26 09:16:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ var PORT = args[0];
 
 app.get('/', function(req, res) 
 {
-	//res.send("Hello");
-	res.status(500).send('Something broke!');
+	res.send("Hello");
 });
 
 var server = app.listen(PORT, function () {
