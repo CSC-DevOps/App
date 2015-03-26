c2bffead6810fa2678cfcef54f641d227c5dcf8c
Chris Parnin <chris.parnin@gmail.com>
2015-03-26 08:28:23 -0400

    500

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ var PORT = args[0];
 
 app.get('/', function(req, res) 
 {
-	res.send("Hello");
+	//res.send("Hello");
+	res.status(500).send('Something broke!');
 });
 
 var server = app.listen(PORT, function () {
